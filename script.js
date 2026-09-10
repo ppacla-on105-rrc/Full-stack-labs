c360acf3ccd96793cc210b378f251d12f9476e09
Philip Pacla-on <ppacla-on105@academic.rrc.ca>
2026-09-10 15:29:45 -0500

    Fs_lab-1.1

---
+++ script.js
@@ -0,0 +1,114 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("current-year").textContent = currentYear;
+
+const departments = [
+    {
+        name: "Administration",
+        employees: [
+            { firstName: "Zoë", lastName: "Robins" },
+            { firstName: "Madeleine", lastName: "Madden" }
+        ]
+    },
+    {
+        name: "Audit",
+        employees: [
+            { firstName: "Josha", lastName: "Sadowski" },
+            { firstName: "Kate", lastName: "Fleetwood" }
+        ]
+    },
+    {
+        name: "Banking Operations",
+        employees: [
+            { firstName: "Priyanka", lastName: "Bose" },
+            { firstName: "Hammed", lastName: "Animashaun" },
+            { firstName: "Álvaro", lastName: "Morte" },
+            { firstName: "Taylor", lastName: "Napier" },
+            { firstName: "Alan", lastName: "Simmonds" }
+        ]
+    },
+    {
+        name: "Communications",
+        employees: [
+            { firstName: "Gil", lastName: "Cardinal" },
+            { firstName: "Richard J.", lastName: "Lewis" }
+        ]
+    },
+    {
+        name: "Corporate Services",
+        employees: [
+            { firstName: "Randy", lastName: "Bradshaw" },
+            { firstName: "Tracey", lastName: "Cook" },
+            { firstName: "Lubomir", lastName: "Mykytiuk" }
+        ]
+    },
+    {
+        name: "Facilities",
+        employees: [
+            { firstName: "Dakota", lastName: "House" },
+            { firstName: "Lori Lea", lastName: "Okemah" },
+            { firstName: "Renae", lastName: "Morrisseau" },
+            { firstName: "Rick", lastName: "Belcourt" }
+        ]
+    },
+    {
+        name: "Financial Services",
+        employees: [
+            { firstName: "Selina", lastName: "Hanusa" },
+            { firstName: "Buffy", lastName: "Gaudry" },
+            { firstName: "Shaneen Ann", lastName: "Fox" },
+            { firstName: "Allan", lastName: "Little" },
+            { firstName: "Danny", lastName: "Rabbit" }
+        ]
+    },
+    {
+        name: "Human Resources",
+        employees: [
+            { firstName: "Jesse Ed", lastName: "Azure" },
+            { firstName: "Stacy", lastName: "Da Silva" },
+            { firstName: "Vladimír", lastName: "Valenta" },
+            { firstName: "Samone", lastName: "Sayeses-Whitney" },
+            { firstName: "Paul", lastName: "Coeur" }
+        ]
+    },
+    {
+        name: "Information Technology",
+        employees: [
+            { firstName: "Graham", lastName: "Greene" },
+            { firstName: "Sandika", lastName: "Evergreen" },
+            { firstName: "Jennifer", lastName: "Rodriguez" }
+        ]
+    },
+    {
+        name: "IT Technician",
+        employees: [
+            { firstName: "Aiyana", lastName: "Littlebear" },
+            { firstName: "Inara", lastName: "Thunderbird" },
+            { firstName: "Kaya", lastName: "Runningbrook" },
+            { firstName: "Elara", lastName: "Firehawk" },
+            { firstName: "Siona", lastName: "Moonflower" },
+            { firstName: "Kaiyu", lastName: "Greywolf" },
+            { firstName: "Ayawamat", lastName: "Nightwind" },
+            { firstName: "Tala", lastName: "Braveheart" },
+            { firstName: "Iniko", lastName: "Stonebear" },
+            { firstName: "Onatah", lastName: "Redhawk" }
+        ]
+    }
+];
+
+const directory = document.getElementById("employee-directory");
+
+departments.forEach(department => {
+    const departmentHeading = document.createElement("h2");
+    departmentHeading.textContent = department.name;
+
+    directory.appendChild(departmentHeading);
+
+    department.employees.forEach(employee => {
+        const employeeParagraph = document.createElement("p");
+
+        employeeParagraph.textContent = employee.firstName + " " + employee.lastName;
+
+        directory.appendChild(employeeParagraph);
+    });
+});
